0a80751d9d73eacf728161d9f74784f98a637709
xyx1926885268 <1926885268@qq.com>
2024-12-27 17:28:45 +0800

    修改代码逻辑，改为多线程功能

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
 # 空文件夹删除器
 
-#### 介绍
+# 介绍
+
 还在为空文件夹太多一个一个删除费时间而烦恼吗？快来试试这款应用吧！
 
-#### 软件架构
+# 打包方法
 
-基于python3.9.6（python3.x皆可）以及os,threading和datetime库编写
+1. 使用 Nuitka 打包为可执行文件
+2. 运行以下命令打包为可执行文件：
 
+```python
+python -m nuitka --lto=no --onefile --standalone --windows-icon-from-ico=tool.ico  .\空文件夹扫描器.py
+```
 
-#### 安装教程
+# 软件架构
 
-1.  安装python3.9.6，双击空文件夹删除器执行.py文件
-3.  输入想扫描的文件夹（如：D:\）,如何按回车键执行（经测试扫描系统盘时间会长）
+基于python3.10.16（python3.x皆可）以及os,threading和datetime库编写
+
+# 安装教程
+
+1.  安装python3.x，运行 空文件夹删除器.py 文件
+2.  输入想扫描的文件夹（如：D:\）,如何按回车键执行
 3.  或者直接下载空文件夹删除器.exe双击运行
 
-#### 参与贡献
+# 参与贡献
 
-1.  [徐清风 (xyx1926885268_xqf) - Gitee.com](https://gitee.com/xyx1926885268_xqf))
+1.  [little_while](https://gitee.com/little_while)
+1.  [xyx1926885268](https://github.com/xyx1926885268)
--- 空文件夹扫描器.py
+++ 空文件夹扫描器.py
@@ -1,42 +1,53 @@
 import os
-import threading
 from datetime import datetime
-from time import strftime
-from time import gmtime
-lists=[]
-ls=[]
-fp=open("folder.log","w+")
-times=0
-def scanFile(filePath):
-	for	path in os.walk(filePath):
-		lists.append(path[0])
-	for i in lists:
-		try:
-			if len(os.listdir(i))!=0:
+import threading
+try:
+	os.remove("folder.log")
+except:
+	pass
+lists = []
+ls = []
+fp = open ( "folder.log", "w+" )
+times = 0
+
+def scanFile (filePath) :
+	for path in os.walk ( filePath ) :
+		lists.append ( path [0] )
+	for i in lists :
+		try :
+			if len ( os.listdir ( i ) ) != 0 :
 				continue
-			else:
-				ls.append(i)
-		except:
+			else :
+				ls.append ( i )
+		except :
 			continue
-	for m in range(len(ls)):
-		for x in ls:
-			os.rmdir(ls[m])
-			with open("folder.log","a+") as f:
-				data =ls[m]
-				f.write(data)
-				f.write('\n')
-			print(datetime.now(),"正在删除",ls[m])
-			ls.clear()
-			scanFile(filePath)
-if __name__ == '__main__':
-	filePath=input("请输入想扫描的路径：")
-	start_time=datetime.now()
-# 	for y in range(5):#开启5条线程
-# 		s = threading.Thread(target=scanFile(filePath))
-# 		s.start()
-	scanFile(filePath)
-	end_time=datetime.now()
-	times=end_time-start_time
-	print("共花费了",times,"s")
-	print("删除完成")
-	os.system("pause")
+	lists.clear()
+	with open ( "folder.log", "a+" ) as f :
+		for m in range ( len ( ls ) ) :
+			for x in ls :
+				os.rmdir ( ls [m] )
+				f.write ( ls [m]+"\n")
+				print ( datetime.now ( ), "正在删除", ls [m] )
+				ls.clear ( )
+				scanFile ( filePath )
+				
+
+if __name__ == '__main__' :
+	filePath = input ( "请输入想扫描的路径：" )
+	start_time = datetime.now ( )
+	threads = []
+	try :
+		for y in range ( 500 ) :  # 开启5条线程
+			s = threading.Thread ( target = scanFile ( filePath ) )
+			threads.append ( s )
+		for t in threads :
+			t.start ( )
+			t.join ( )
+	except:
+		print("线程开启失败")
+	# scanFile(filePath)
+	end_time = datetime.now ( )
+	times = end_time - start_time
+	print ( "共花费了", times, "s" )
+	print ( "删除完成" )
+	os.system ( "pause" )
